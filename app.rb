393dcb58c29c55ce0d9a135e3305d435266570e4
samanthachasalow <samanthachasalow@chatham-nj.org>
2018-08-14 20:25:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get"/" do
+    "Hello, World!"
+  get"/name" do 
+    "My name is __"
+  get "/hometown" do
+    "My hometown is__"
+  get "/favorite-song" do
+    "My favorite song is__"
+end
 end
